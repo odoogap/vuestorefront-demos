cd15811488499b1abd3cd77ea438bd60eb9e57cc
Leonardo Muniz <lmuniz@odoogap.com>
2022-02-03 14:29:39 -0300

    Starting demo-1

---
+++ demo-1/middleware.config.js
@@ -0,0 +1,15 @@
+const odooBaseUrl = process.env.BACKEND_BASE_URL || process.env.BASE_URL || 'https://vsfdemo.labs.odoogap.com/';
+const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
+
+module.exports = {
+  integrations: {
+    odoo: {
+      location: '@vue-storefront/odoo-api/server',
+      configuration: {
+        odooBaseUrl,
+        graphqlBaseUrl
+      }
+    }
+  }
+};
+
