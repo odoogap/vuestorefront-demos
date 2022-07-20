1aaea1b6839ae8072d2e19e308224f1135df03c0
Eliyas Hossain <ekhan@odoogap.com>
2022-07-20 13:23:52 +0600

    [IMP] sitemap generator

--- demo-1/middleware.config.js
+++ demo-1/middleware.config.js
@@ -1,9 +1,10 @@
-const formatedBaseUrl = process.env.BASE_URL && process.env.BASE_URL.slice(-1) == '/' ? 
-                          process.env.BASE_URL : 
-                          process.env.BASE_URL + '/';
+const formatedBaseUrl = process.env.BASE_URL && process.env.BASE_URL.slice(-1) == '/' ?
+  process.env.BASE_URL :
+  process.env.BASE_URL + '/';
 
 const odooBaseUrl = formatedBaseUrl || 'https://vsfdemo.labs.odoogap.com/';
 const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
+const baseDomain = process.env.BASE_DOMAIN || 'vsfdemo.labs.odoogap.com'
 
 module.exports = {
   integrations: {
@@ -11,7 +12,8 @@ module.exports = {
       location: '@vue-storefront/odoo-api/server',
       configuration: {
         odooBaseUrl,
-        graphqlBaseUrl
+        graphqlBaseUrl,
+        baseDomain,
       }
     }
   }
